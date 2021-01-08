7b220bbbef887aa85e5642894c77a975bbb2e688
zhouying <1770385837@qq.com>
2021-01-08 20:26:29 +0800

    添加ToastUtils工具类

---
+++ androidutils/src/main/java/com/dream/androidutils/ToastUtils.java
@@ -0,0 +1,24 @@
+package com.dream.androidutils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * function: Toast工具类
+ *
+ * @author zy
+ * @since 1/8/21
+ */
+public class ToastUtils {
+
+    /**
+     * 展示短时间Toast
+     * 
+     * @param mContext 上下文
+     * @param mShowText 展示的内容
+     */
+    public static void showShortToast(Context mContext,String mShowText){
+        Toast.makeText(mContext, mShowText, Toast.LENGTH_SHORT).show();
+    }
+
+}
