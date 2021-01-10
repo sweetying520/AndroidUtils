0af14d4c647beeb1104236bfe03afb4009cc3e4a
zhouying <1770385837@qq.com>
2021-01-10 16:41:55 +0800

    版本升级

--- app/src/main/java/com/dream/androidutils/MainActivity.java
+++ app/src/main/java/com/dream/androidutils/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //ToastUtils.showShortToast(this,"erdai666");
+        ToastUtils.showShortToast(this,"erdai666");
 
     }
 }
