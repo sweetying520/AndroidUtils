5ef5c995690eed63624011426c159b580f2eefe2
zhouying <1770385837@qq.com>
2022-10-29 23:40:06 +0800

    feat: 修改 StringUtils Api

--- androidutils/src/main/java/com/dream/androidutils/StringUtils.java
+++ androidutils/src/main/java/com/dream/androidutils/StringUtils.java
@@ -9,16 +9,18 @@ package com.dream.androidutils;
 public class StringUtils {
 
     /**
-     * 获取字符串的长度
+     * 获取字符串的字节数组
      */
-    public static int getStringLength(String str){
-        return str.length();
+    public static byte[] getByteArray(String str){
+        if(str == null)return new byte[0];
+        return str.getBytes();
     }
 
     /**
      * 将字符串转换成字符数组
      */
-    public static char[] getChars(String str){
+    public static char[] getCharArray(String str){
+        if(str == null)return new char[0];
         return str.toCharArray();
     }
 }
