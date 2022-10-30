71ef7eca21c79ce4a9b82b363ba4ff45abe0e5ab
zhouying <1770385837@qq.com>
2022-10-30 09:41:39 +0800

    feat: StringUtils 相关 Api 删除

--- androidutils/src/main/java/com/dream/androidutils/StringUtils.java
+++ androidutils/src/main/java/com/dream/androidutils/StringUtils.java
@@ -9,13 +9,6 @@ package com.dream.androidutils;
 public class StringUtils {
 
     /**
-     * 获取字符串的字节数组
-     */
-    public static byte[] getByteArray(String str){
-        return str.getBytes();
-    }
-
-    /**
      * 将字符串转换成字符数组
      */
     public static char[] getCharArray(String str){
